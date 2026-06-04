a98538e507d80a3d5db7dc3b7b8e30b7df22da1d
NicolasOui <qellylight@gmail.com>
2026-06-05 01:01:59 +0300

    Delete src directory

--- src/main/java/api/Endpoints.java
+++
@@ -1,8 +0,0 @@
-package api;
-
-public class Endpoints {
-    public static final String BASE_URL = "https://stellarburgers.education-services.ru";
-    public static final String USER_REGISTER = "/api/auth/register";
-    public static final String DELETE_USER = "/api/auth/user";
-    public static final String USER_LOGIN = "/api/auth/login";
-}
